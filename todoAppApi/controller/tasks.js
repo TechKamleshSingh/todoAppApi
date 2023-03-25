30fee24db8ddb0de79b16febf7133a7a29d549ee
tksguide8@gmail.com <tksguide8@gmail.com>
2023-03-25 15:40:36 +0530

    Host Api

--- todoAppApi/controller/tasks.js
+++ todoAppApi/controller/tasks.js
@@ -1,4 +1,5 @@
 import Tasks from "../models/Tasks.js";
+import createError from "../utils/createError.js";
 export const createTasks = async (req, res, next) => {
   try {
     const newTask = new Tasks({
@@ -27,6 +28,44 @@ export const myTasks = async (req, res, next) => {
   try {
     const myTasks = await Tasks.find({ user: req.user.id });
     return res.status(200).json(myTasks);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const updateTask = async (req, res, next) => {
+  try {
+    const task = await Tasks.findById(req.params.taskId).exec();
+    if (!task)
+      return next(createError({ status: 404, message: "No task found" }));
+    if (task.user.toString() !== req.user.id)
+      return next(createError({ status: 401, message: "it's not your task" }));
+
+    const updatedTask = await Tasks.findByIdAndUpdate(
+      req.params.taskId,
+      {
+        title: req.body.title,
+        completed: req.body.completed,
+      },
+      { new: true }
+    );
+    return res.status(200).json(updatedTask);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const deteteTask = async (req, res, next) => {
+  try {
+    const task = await Tasks.findById(req.params.taskId).exec();
+    if (!task)
+      return next(createError({ status: 404, message: "No task found" }));
+
+    if (task.user.toString() !== req.user.id)
+      return next(createError({ status: 401, message: "It's not your task" }));
+
+    await Tasks.findByIdAndDelete(req.params.taskId);
+    return res.status(200).json({ message: "Task deleted" });
   } catch (error) {
     return next(error);
   }
--- todoAppApi/routes/task.js
+++ todoAppApi/routes/task.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createTasks, getTasks, myTasks } from "../controller/tasks.js";
+import { createTasks, deteteTask, getTasks, myTasks, updateTask } from "../controller/tasks.js";
 
 const router = express.Router();
 
-router.post('/', createTasks)
-router.get('/', getTasks)
-router.get('/mytasks', myTasks)
+router.post("/", createTasks);
+router.get("/", getTasks);
+router.get("/mytasks", myTasks);
+router.put("/:taskId", updateTask)
+router.delete("/:taskId", deteteTask)
 
 export default router;
